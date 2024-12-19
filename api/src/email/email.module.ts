4c027764cb7ec4f30f45ee9296d1a3481a8da89f
Emil Ebert <emilebert@icloud.com>
2024-12-19 20:15:02 +0100

    export email service

--- api/src/email/email.module.ts
+++ api/src/email/email.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
-import { EmailController } from './email.controller';
-import { EmailService } from './email.service';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import {Module} from '@nestjs/common';
+import {EmailController} from './email.controller';
+import {EmailService} from './email.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 import {EmailEntity} from "./entity/Email.entity";
 import {TargetEntity, TargetVariableEntity} from "./entity/Target.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EmailEntity, TargetEntity, TargetVariableEntity])],
-  controllers: [EmailController],
-  providers: [EmailService]
+    imports: [TypeOrmModule.forFeature([EmailEntity, TargetEntity, TargetVariableEntity])],
+    controllers: [EmailController],
+    providers: [EmailService],
+    exports: [EmailService]
 })
-export class EmailModule {}
+export class EmailModule {
+}
